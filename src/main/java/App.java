b6fe53663970cca2854d135111e837381b86fcea
Reed Lambier <rlambier46@gmail.com>
2016-05-13 19:38:12 -0700

    add finished backend testing and logic. Beginning front end logic and testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,14 +5,14 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-  //   staticFileLocation("/public");
-  //   String layout = "templates/layout.vtl";
-  //
-  //   get("/", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("template", "templates/index.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   //
   //   get("/categories", (request, response) -> {
   //     HashMap<String, Object> model = new HashMap<String, Object>();
