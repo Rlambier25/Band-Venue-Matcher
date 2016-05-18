568a1e1bb3b6b0f9645c9cd2649e9ab322316e96
Reed Lambier <rlambier46@gmail.com>
2016-05-18 08:50:21 -0700

    Adding walk through for routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -45,18 +45,36 @@ public class App {
     });
 
     get("/bands/:id", (request,response) -> {
+      //Hashmap is where the model stores the Strings and Objects
       HashMap<String, Object> model = new HashMap<String, Object>();
+      //band is the instance of Band. find() is what locates teh specific
+      //id for which ever band is clicked on or being viewed.
       Band band = Band.find(Integer.parseInt(request.params("id")));
+      //model.put broadcasts band which has a placeholder of "band".
+      //this talks to the front end templates in the vtl files. $band is referring to this
+      //place holder.
       model.put("band", band);
+      //Venue.all() has a place holder of "allVenues". The purpose of this is to
+      //find all instances of Venue in the model object. This specific one is for
+      //creating <option>'s in the select box on the band.vtl page.
       model.put("allVenues", Venue.all());
+      //this is referring to the band.vtl page underneath templates.
+      //This directs the request,response to display the specific page.
       model.put("template", "templates/band.vtl");
+      //the model is a Map, allowing the use of multiple objects keyed by name.
       return new ModelAndView(model, layout);
+      //New instance of the VelocityTemplateEngine
     }, new VelocityTemplateEngine());
 
+
+    //This get request is virtuall the same as above except for Venues. Instead of finding instances
+    //of "allVenues" it will find "allBands" associated with the specific venues.
     get("/venues/:id", (request,response) ->{
       HashMap<String, Object> model = new HashMap<String, Object>();
       Venue venue = Venue.find(Integer.parseInt(request.params("id")));
       model.put("venue", venue);
+      //model is a map, it consists of model attributes (individual objects). These createQuery
+      //constructors for creating a model with the listed attributes.
       model.put("allBands", Band.all());
       model.put("template", "templates/venue.vtl");
       return new ModelAndView(model, layout);
@@ -69,6 +87,7 @@ public class App {
       Band band = Band.find(bandId);
       venue.addBand(band);
       response.redirect("/venues/" + venueId);
+      //returning null is used for returning no data.
       return null;
     });
 
@@ -106,9 +125,6 @@ public class App {
       response.redirect("/bands");
       return null;
     });
-
-
-
 
   }
 }
